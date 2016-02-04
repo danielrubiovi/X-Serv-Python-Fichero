d56b75eac7230abaf1aac29b37e8188cf021284b
Daniel Rubio Villegas <drubio@zeta40.aulas.gsyc.es>
2016-02-04 17:34:45 +0100

    Ejercicio 13.4

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+fich = open ("/etc/passwd", 'r')
+lineas = fich.readlines()
+
+for line in lineas:
+	lista_lineas = line.split(':')
+	print lista_lineas[0], lista_lineas[-1] [:-1]
+	
+print "\nNumero de lineas:", len(lineas)
+	
 
